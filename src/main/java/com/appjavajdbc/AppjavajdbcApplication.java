bbee081d237eaca342b8e1684b1e2c74f5c186da
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-07-22 14:35:53 -0500

    subiendo proyecto

---
+++ src/main/java/com/appjavajdbc/AppjavajdbcApplication.java
@@ -0,0 +1,51 @@
+package com.appjavajdbc;
+
+import com.appjavajdbc.dao.CategoryDao;
+
+
+import com.appjavajdbc.entity.Category;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+
+@SpringBootApplication
+public class AppjavajdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppjavajdbcApplication.class, args);
+
+
+/* Era solo para ensayar la conexión a la base de datos
+		try {
+			ConnectionCore connection = new ConnectionCore();
+			connection.getConnection();
+			System.out.println("Conexión establecida correctamente.");
+
+		}catch (Exception e) {
+			System.out.println("Error al conectar con la base de datos: " + e.getMessage());
+			e.printStackTrace();
+		}
+*/
+
+		CategoryDao categoryDao = new CategoryDao();
+
+		List<Category>  categories = categoryDao.findAll();
+
+		System.out.println("Categories  start: ");
+		for (Category category: categories){
+			System.out.println("Id: " +  category.getId());
+			System.out.println("Name: " + category.getName());
+			System.out.println("Description: " + category.getDescription());
+			System.out.println("Url key: " + category.getUrlKey());
+			System.out.println("State: " + category.getState());
+			System.out.println("Created at: " + category.getCreatedAt());
+			System.out.println();
+		}
+
+		System.out.println("Categories  end.");
+
+	}
+
+}
---
+++ src/test/java/com/appjavajdbc/AppjavajdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appjavajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjavajdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
