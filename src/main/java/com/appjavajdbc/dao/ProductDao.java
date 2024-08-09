22fb58acf8f2edcd67dc1a8f8e56b093b55c2338
Andres Muñoz Taborda <andreslx11@gmail.com>
2024-08-09 14:33:50 -0500

    add product

---
+++ src/main/java/com/appjavajdbc/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.appjavajdbc.dao;
+
+
+import com.appjavajdbc.dao.core.CrudDao;
+import com.appjavajdbc.entity.Product;
+
+public interface ProductDao  extends CrudDao<Product, Long> {
+}
